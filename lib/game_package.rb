9f7583e66b4e87f0b44e669066ca23301dc36e29
EC2 Default User <ec2-user@ip-172-31-13-177.ap-northeast-1.compute.internal>
2020-06-13 15:25:09 +0000

    first commit

---
+++ lib/game_package.rb
@@ -0,0 +1,6 @@
+require "game_package/version"
+
+module GamePackage
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/game_package/version.rb
@@ -0,0 +1,3 @@
+module GamePackage
+  VERSION = "0.1.0"
+end
