c8dd70bdd01804d212a5c29c1e27f6f030d025be
EC2 Default User <ec2-user@ip-172-31-13-177.ap-northeast-1.compute.internal>
2020-06-13 15:30:45 +0000

    create janken

--- lib/game_package.rb
+++ lib/game_package.rb
@@ -1,6 +1,1 @@
-require "game_package/version"
-
-module GamePackage
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require 'game_package/janken'
---
+++ lib/game_package/janken.rb
@@ -0,0 +1,61 @@
+require "game_package/version"
+
+module GamePackage
+  module Janken
+    class Error < StandardError; end
+      
+    GU = 0
+    TYOKI = 1
+    PA = 2
+    
+    class << self
+      def start
+        puts "ジャンケン..."
+        jadgement = 'draw'
+
+        while jadgement == 'draw' do
+          player = player_hand
+          computer = computer_hand
+          jadgement = jadge(player, computer)
+          puts "ポン\n"
+          puts '-'*15
+          puts "あなた #{convert_hand_into_string(player)}\nあいて #{convert_hand_into_string(computer)}"
+          puts '-'*15
+          puts ''
+          return  puts 'あなたの勝ちです'  if  jadgement == 'win'
+          return puts 'あなたの負けです' if jadgement == 'lose'
+          
+          puts 'あいこで...'
+        end
+      end
+  
+      def player_hand
+        puts "\nあなたの出す手を決めてください"
+        puts '-'*15
+        puts "グー(0)\nチョキ(1)\nパー(2)"
+        puts '-'*15
+        gets.to_i
+      end
+      
+      def jadge(player_hand, computer_hand)
+        return 'draw' if player_hand == computer_hand
+          
+        return 'win' if player_hand == GU && computer_hand == TYOKI ||
+          player_hand == TYOKI && computer_hand == PA ||
+          player_hand == PA && computer_hand == GU
+        
+        'lose'
+      end
+      
+      def computer_hand
+        [GU, TYOKI, PA].sample
+      end
+      
+      def convert_hand_into_string(hand)
+        ['グー', 'チョキ', 'パー'][hand]
+      end
+    end
+    
+    private_class_method :player_hand, :jadge, :computer_hand, :convert_hand_into_string
+  end
+end
